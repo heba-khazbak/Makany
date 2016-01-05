0b13d28c617d115027376da9bd788b89b7bffb15
ziad.mohamed <ziad_mohamed9473@hotmail.com>
2016-01-05 23:24:41 +0200

    makany project

---
+++ MakanyProject/src/com/eg/Makany/Models/Post.java
@@ -0,0 +1,42 @@
+package com.eg.Makany.Models;
+
+import java.util.Date;
+import java.util.List;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+
+public class Post {
+	private String content;
+
+	public Post(String content) {
+		this.content = content;
+
+	}
+
+
+	public Boolean savePost() {
+		DatastoreService datastore = DatastoreServiceFactory
+				.getDatastoreService();
+		Query gaeQuery = new Query("posts");
+		PreparedQuery pq = datastore.prepare(gaeQuery);
+		List<Entity> list = pq.asList(FetchOptions.Builder.withDefaults());
+
+		Entity post = new Entity("posts", list.size() + 1);
+
+		post.setProperty("content", this.content);
+		datastore.put(post);
+
+		return true;
+
+	}
+}
